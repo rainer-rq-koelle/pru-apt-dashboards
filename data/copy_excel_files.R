88c74a421020bd8a8674afba9491b2a8d7e5b593
Enrico Spinielli <enrico.spinielli@eurocontrol.int>
2020-09-08 17:55:57 +0200

    script to copy Excel files around

---
+++ data/copy_excel_files.R
@@ -0,0 +1,27 @@
+# copy over the Excel files 
+
+library(fs)
+
+# AIRPORT from AIU Portal
+basedir <- "C:/Users/spi/repos/pru-portal/static/download/xls"
+filenames <- c(
+  "Airport_Traffic.xlsx",
+  "Airport_Arrival_ATFM_Delay.xlsx",
+  "ATFM_Slot_Adherence.xlsx",
+  "ASMA_Additional_Time.xlsx",
+  "Taxi-Out_Additional_Time.xlsx"
+)
+
+files_in <- fs::path_abs(filenames, start = basedir)
+files_out <- fs::path_abs(filenames, start = here::here("data"))
+
+fs::file_copy(files_in, files_out, overwrite = TRUE)
+
+
+# AIRPORT from COVID
+basedir <- 'G:/HQ/dgof-pru/Data/DataProcessing/Covid19'
+file_in <- fs::path_abs("1_Top_100_Airport_dep+arr_traffic_LTFM+LTBA (Synthesis).xlsx", start = basedir)
+file_out <- fs::path_abs("COVID-AIRPORT.xlsx", start = here::here("data"))
+
+fs::file_copy(file_in, file_out, overwrite = TRUE)
+
