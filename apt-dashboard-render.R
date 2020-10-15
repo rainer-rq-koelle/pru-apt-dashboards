fbac0bbead6de413fc087a0d42ea6039612c5d6f
Enrico Spinielli <enrico.spinielli@eurocontrol.int>
2020-10-15 11:10:33 +0200

    slammed utility finction in their own file

--- apt-dashboard-render.R
+++ apt-dashboard-render.R
@@ -38,6 +38,8 @@ min_year <- 2016 # by definition 5 years, i.e. 2016-2020
 # TO-DO TROUBLE SHOOTING ----------------------------------------
 # THE FOLLOWING AIRPORT DBs DO NOT RENDER - CHECK WHAT THROWS ERRORS
 nope <- c("EGNT","ENBR","ENVA","ENZV","GCFV","LCLK","LFBO","LFML","LIMF","WSSS")
+# nope <- c("XXXX")
+#apts <- list.files("./data-ad-rwy-charts/", pattern = "[A-Z]{4}\\.png") %>% strtrim(4)
 apts <- list.files("./data-ad-charts/", pattern = "[A-Z]{4}\\.png") %>% strtrim(4)
 apts <- setdiff(apts, nope)
 
@@ -127,156 +129,29 @@ covid_df <- readr::read_csv("./data/COVID_AIRPORT.csv")
 
 ## ------------ UTILITY FUNCTIONS -------------------------------------
 #
-filter_df_by_apt <- function(.df, .apt){
-  df <- .df %>% filter(APT_ICAO == .apt) %>%
-    filter(YEAR >= min_year)  # ensure only 5 years of data
-}
-#
-pick_apt_name <- function(.df, .apt){
-  name <- .df %>% filter(APT_ICAO == .apt)
-  name <- name$APT_NAME[1]
-}
-#
-pick_state_name <- function(.df, .apt){
-  state <- .df %>% filter(APT_ICAO == .apt)
-  state <- state$STATE_NAME[1]
-}
-#
-pick_apt_iata <- function(.df, .apt){
-  iata <- .df %>% filter(APT_ICAO == .apt)
-  iata <- iata$APT_IATA[1]
-}
-#
-landing_page_indicators <- function(.df=db_df, .atfm=atfm_df, .apt){
-  inds <- .df %>% filter(APT_ICAO == .apt)
-  
-  ind_tfc_2019 <- inds %>%
-    select(APT_ICAO, YEAR, NB_NM_TOT) %>% filter(YEAR == 2019) %>%
-    group_by(APT_ICAO, YEAR) %>%
-    summarise(NB_NM_TOT = sum(NB_NM_TOT, na.rm = TRUE)) %>% ungroup()
-  
-  ind_txot_2019 <- inds %>% filter(YEAR == 2019) %>%
-    group_by(APT_ICAO, YEAR) %>%
-    summarise( ADD_TAXI_OUT_TIME_MIN = sum(ADD_TAXI_OUT_TIME_MIN, na.rm = TRUE)
-               ,NB_TAXI_OUT_FL        = sum(NB_TAXI_OUT_FL,        na.rm = TRUE)
-    )%>% ungroup() %>%
-    mutate(AVG_ADD_TXOT = round(ADD_TAXI_OUT_TIME_MIN / NB_TAXI_OUT_FL,2) ) %>%
-    select(AVG_ADD_TXOT)
-  
-  ind_asma_2019 <- inds %>% filter(YEAR == 2019) %>%
-    group_by(APT_ICAO, YEAR) %>%
-    summarise( ADD_ASMA_TIME_MIN = sum(ADD_ASMA_TIME_MIN, na.rm = TRUE)
-               ,NB_ASMA_FL       = sum(NB_ASMA_FL,        na.rm = TRUE)
-    )%>% ungroup() %>%
-    mutate(AVG_ADD_ASMA = round(ADD_ASMA_TIME_MIN / NB_ASMA_FL,2) ) %>%
-    select(AVG_ADD_ASMA)
-  
-  ind_atfm_2019 <- .atfm %>% filter(APT_ICAO == .apt, YEAR == 2019) %>%
-    select(FLT_ARR_1, DLY_APT_ARR_1) %>%
-    summarise( FLT_ARR_1     = sum(FLT_ARR_1,     na.rm = TRUE)
-               ,DLY_APT_ARR_1 = sum(DLY_APT_ARR_1, na.rm = TRUE)) %>%
-    mutate(AVG_ARR_ATFM = round(DLY_APT_ARR_1 / FLT_ARR_1,2) ) %>%
-    select(AVG_ARR_ATFM)
-  
-  out <- ind_tfc_2019 %>%
-    bind_cols(ind_txot_2019, ind_asma_2019, ind_atfm_2019)
-}
-#
-latest_month_indicators <- function(.df=db_df, .atfm=atfm_df, .apt){
-  inds <- .df %>% filter(APT_ICAO == .apt)
-  
-  mth_name <- c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
-  
-  ind_tfc_lm <- inds %>%
-    select(APT_ICAO, YEAR, MONTH_NUM, NB_NM_TOT) %>% na.omit() %>%
-    filter(YEAR == max(YEAR)) %>% filter(MONTH_NUM == max(MONTH_NUM)) %>%
-    mutate( MONTH = mth_name[MONTH_NUM]
-            ,  TFC = paste0(NB_NM_TOT," (", MONTH, " ", YEAR,")")
-    ) %>%
-    select(APT_ICAO, TFC)
-  
-  ind_txot_lm <- inds %>%
-    select(APT_ICAO, YEAR, MONTH_NUM, ADD_TAXI_OUT_TIME_MIN, NB_TAXI_OUT_FL) %>%
-    na.omit() %>% filter(YEAR == max(YEAR) ) %>%
-    filter(MONTH_NUM == max(MONTH_NUM)) %>%
-    mutate(AVG_ADD_TXOT = round(ADD_TAXI_OUT_TIME_MIN / NB_TAXI_OUT_FL,2)
-           ,AVG_ADD_TXOT= paste0(AVG_ADD_TXOT," (", mth_name[MONTH_NUM], " ", YEAR,")")
-    ) %>%
-    select(AVG_ADD_TXOT)
-  
-  ind_asma_lm <- inds %>%
-    select(APT_ICAO, YEAR, MONTH_NUM, ADD_ASMA_TIME_MIN, NB_ASMA_FL) %>%
-    na.omit() %>% filter(YEAR == max(YEAR) ) %>%
-    filter(MONTH_NUM == max(MONTH_NUM)) %>%
-    mutate( AVG_ADD_ASMA = round(ADD_ASMA_TIME_MIN / NB_ASMA_FL, 2)
-            ,AVG_ADD_ASMA = paste0(AVG_ADD_ASMA," (", mth_name[MONTH_NUM], " ", YEAR,")")
-    ) %>%
-    select(AVG_ADD_ASMA)
-  
-  ind_atfm_lm <- .atfm %>% filter(APT_ICAO == .apt) %>%
-    select(YEAR, MONTH_NUM, FLT_ARR_1, DLY_APT_ARR_1) %>%
-    filter(YEAR == max(YEAR)) %>% filter(MONTH_NUM == max(MONTH_NUM)) %>%
-    na.omit() %>%
-    group_by(YEAR, MONTH_NUM) %>%
-    summarise( FLT_ARR_1     = sum(FLT_ARR_1, na.rm = TRUE)
-               ,DLY_APT_ARR_1 = sum(DLY_APT_ARR_1, na.rm = TRUE)) %>%
-    mutate( AVG_ARR_ATFM = round(DLY_APT_ARR_1 / FLT_ARR_1, 2)
-            ,AVG_ARR_ATFM = paste0(AVG_ARR_ATFM," (", mth_name[MONTH_NUM], " ", YEAR,")")) %>%
-    select(AVG_ARR_ATFM)
-  
-  inds_lm <- ind_tfc_lm %>%
-    bind_cols(ind_txot_lm, ind_asma_lm, ind_atfm_lm)
-  
-}
-#
-trim_covid <- function(.df, .apt){
-  df <- .df %>% filter(APT_ICAO == .apt) %>%
-    select(DAY, FLTS_2020, FLTS_2019, MOV_AVG_WK)
-}
-#
-pack_thru <- function(.df, .apt){
-  df <- .df %>% dplyr::filter(APT_ICAO == .apt) %>%
-    dplyr::mutate(
-      # DATE = lubridate::dmy(DAY, tz="UTC")
-      DATE = DAY
-      ,YEAR = year(DATE), MONTH_NUM = month(DATE)
-     , WEEKDAY = lubridate::wday(DATE, label=TRUE)) %>%
-    dplyr::filter(YEAR == max(YEAR)) %>% filter(MONTH_NUM == max(MONTH_NUM)) %>%
-    dplyr::select(APT_ICAO, YEAR, MONTH_NUM, DATE, WEEKDAY, TIME, ROLLING_HOUR_MVT, PHASE) %>%
-    dplyr::group_by(YEAR, MONTH_NUM, TIME, PHASE) %>% summarise(ROLLING_HOUR_MVT = mean(ROLLING_HOUR_MVT)) %>%
-    dplyr::ungroup()
-}
+source(here::here("R", "utils.R"), encoding = "UTF8")
+
 #
 ## ------------ RENDER DASHBOARDS -------------------------------------
+## TEST ##
+apt <- "EBBR"
+p <- prepare_params(apt)
+rmarkdown::render(
+  input  = "apt-dashboard.Rmd",   # master flexdashboard Rmd
+  params = p,
+  output_file = here::here("docs", paste0(apt, ".html"))
+)
+
 
 # apt_range <- 2:2
+apt_range <- c(1, 5, 7)
 apt_range <- 1:length(apts)
-# apt_range <- c(1, 5, 7)
 apts %>%
   magrittr::extract(apt_range) %>%
   purrr::walk(
     .f=~rmarkdown::render(
       input  = "apt-dashboard.Rmd"   # master flexdashboard Rmd
-      , params = list( #------ start params -------------------------
-                       icao  = .
-                       ,iata  = pick_apt_iata(   db_df ,   .apt = .)   # merge iata code with other source
-                       ,name  = pick_apt_name(   tfc_df,   .apt = .)
-                       ,state = pick_state_name( tfc_df,   .apt = .)
-                       ,config= filter_df_by_apt(config_df,.apt = .)
-                       ,ldgsum= landing_page_indicators(db_df, atfm_df, .apt = .)
-                       ,latest= latest_month_indicators(db_df, atfm_df, .apt = .)
-                       ,covid = trim_covid(      covid_df, .apt = .)
-                       ,tfc   = filter_df_by_apt(tfc_df,   .apt = .)
-                       ,thru  = pack_thru(       thru_df,  .apt = .)
-                       ,atfm  = filter_df_by_apt(atfm_df,  .apt = .)
-                       ,slot  = filter_df_by_apt(slot_df,  .apt = .)
-                       ,asma  = filter_df_by_apt(asma_df,  .apt = .)
-                       ,txot  = filter_df_by_apt(txot_df,  .apt = .)
-                       ,txit  = filter_df_by_apt(txit_df,  .apt = .)
-                       ,pddly = filter_df_by_apt(pddly_df, .apt = .)
-                       ,turn  = filter_df_by_apt(turn_df,  .apt = .)
-                      # ,punc  = filter_df_by_apt(punc_df,  .apt = .)
-      ) #----------------- end params ---------------------------
+      , params = prepare_params(.)
       # output_dir DEACTIVATED and included in output_file name
       # brittle as reported in stackoverflow #  , output_dir = "./boards"
       #, output_file = paste0("./boards/", ., ".html")
--- data/copy_excel_files.R
+++ data/copy_excel_files.R
@@ -34,3 +34,50 @@ file_out <- fs::path_abs("COVID-AIRPORT.xlsx", start = here::here("data"))
 
 fs::file_copy(file_in, file_out, overwrite = TRUE)
 
+# process COVID data
+library(readxl)
+library(dplyr)
+library(readr)
+ds <- readxl::read_excel(
+  path  = here::here("data", "COVID-AIRPORT.xlsx"),
+  sheet = "DATA",
+  range = cell_cols("O:X"),
+  skip = 10
+)
+
+nms <- readxl::read_excel(
+  path  = here::here("data", "COVID-AIRPORT.xlsx"),
+  sheet = "MAP_STATE_AIRPORT",
+  range = cell_cols("B:D")
+) %>%
+  # add header col to find and filter empty cells not captured correctly
+  # du to merge cell (i.e. skip = x) stumbles over cell with formula
+  mutate(
+    HEADER = if_else(.[[1]] == "ARP_NAME", 1, 0),
+    HEADER = tidyr::replace_na(HEADER, 0),
+    HEADER = cumsum(HEADER)
+  ) %>%
+  filter(HEADER == 1)
+
+# delete column
+nms$HEADER <- NULL
+
+# now make first row the header row and remove it
+names(nms) <- nms[1, ]
+nms <- nms[-1, ]
+
+ds <- ds %>%
+  rename(
+    "ARP_NAME" = "Entity", "WEEK" = "Week", "DAY" = "Day",
+    "FLTS_2019" = "Flights 2019", "FLTS_2020" = "Flights 2020",
+    "VAR_DLY" = "Daily Variation", "VAR_WKLY" = "Weekly Variation",
+    "MOV_AVG_WK" = "Weekly Moving Average"
+    # ,"DAY2019" = "...11"
+)
+
+# merge ICAO with ds
+ds <- ds %>%
+  left_join(nms, by = "ARP_NAME") %>%
+  rename(APT_ICAO = ARP_CODE)
+
+write_csv(ds, here::here("data", "COVID_AIRPORT.csv"))
